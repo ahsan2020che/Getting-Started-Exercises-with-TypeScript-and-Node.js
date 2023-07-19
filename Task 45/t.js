5ac5d3047e7903f915e562566e354c12bfed7265
ahsan2020che <ahsan2020che@gmail.com>
2023-07-19 13:30:57 +0500

    Take 45 updated

---
+++ Task 45/t.js
@@ -0,0 +1,5 @@
+function car_model(a, b, c, d) {
+    console.log("You enteres a car with following details:\n            Car Name       Car Manufactuer      Color          Engine Size  \n              ".concat(a, "             ").concat(b, "           ").concat(c, "              ").concat(d));
+}
+car_model('civic', 'Honda');
+car_model('civic', 'Honda', 'Black', '1200cc');
---
+++ Task 45/t.ts
@@ -0,0 +1,7 @@
+function car_model(a,b,c?,d?){
+    console.log(`You enteres a car with following details:
+            Car Name       Car Manufactuer      Color          Engine Size  
+              ${a}             ${b}           ${c}              ${d}`)
+}
+car_model('civic','Honda')
+car_model('civic','Honda','Black','1200cc')
